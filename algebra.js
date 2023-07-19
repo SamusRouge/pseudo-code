bee4b666fe0226804e466aeca52f3ff394926e31
✪⋆⋇☪ʌɲɲʌɦ δμκз☪⋇⋆✪ <asj@utp.edu.co>
2023-07-19 12:48:41 -0500

    algebra statements on js

---
+++ algebra.js
@@ -0,0 +1,39 @@
+// como parte de estudio del libro de Seymour Lipschutz 
+// se propone escribir formulas en BASIC para diferentes expresiones algebraicas
+// como ejercicio se excriben las mismas expresiones algebraicas en JavaScript
+// Fuente:Lipschutz, S. (1992) Algoritmos, diagramas de flujo, programas n
+// pseudocodigo en McGraw-Hill(E.) Matematicas para la computacion (1era Ed., p 125)
+
+
+// literal (a)
+var x = 1;
+var y =2;
+var resultadoa = x**2+y**2;
+console.log (resultadoa);
+
+// literal (b)
+var resultadob = (x+y)**2
+console.log (resultadob);
+
+// literal (c)
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var resultadoc= (2*a*b)/(c+d);
+console.log (resultadoc);
+
+//literal (d)
+var t=2;
+var n=2;
+var resultadod= t**(n+1);
+console.log (resultadod);
+
+// literal (e)
+var z =3;
+var resultadoe= x/(y*z);
+console.log (resultadoe);
+
+//literal (f)
+var resultadof = (x**2+2*x*y-y**2)**5; 
+console.log = resultadof; 
